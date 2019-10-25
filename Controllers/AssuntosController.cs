c8905bda217e6b518df80306465bbb5c5074a570
Diogo Conti <diogosconti@gmail.com>
2019-10-25 17:31:56 -0300

    Versão Git 2
    Assuntos Controller e View

---
+++ Controllers/AssuntosController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Getec.PortalSef.Database;
+using Getec.PortalSef.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Getec.PortalSef.Admin.Web.Controllers
+{
+    public class AssuntosController : Controller
+    {
+        private readonly SefDbContext _db;
+
+        public AssuntosController(SefDbContext context)
+        {
+            _db = context;
+        }
+        // GET: Assuntos
+        public IActionResult Index()
+        {
+            var assuntos = _db.Assuntos.ToList();
+            ViewBag.Entidades = _db.Entidades.ToList();
+            ViewBag.Gerencias = _db.Gerencias.ToList();
+            ViewBag.Sistemas = _db.Sistemas.ToList();
+            ViewBag.Modulos = _db.Modulos.ToList();
+            return View();
+        }
+
+        // GET: Assuntos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Assuntos/Create
+        public IActionResult Create()
+        {
+            var assuntos = _db.Assuntos.ToList();
+            ViewBag.Entidades = _db.Entidades.ToList();
+            ViewBag.Gerencias = _db.Gerencias.ToList();
+            ViewBag.Sistemas = _db.Sistemas.ToList();
+            ViewBag.Modulos = _db.Modulos.ToList();
+            return View(assuntos);
+        }
+
+        // POST: Assuntos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Assuntos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Assuntos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Assuntos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Assuntos/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Getec.PortalSef.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -33,6 +35,13 @@ namespace Getec.PortalSef.Admin.Web
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            ////------ configuração Mysql
+            services.AddDbContext<SefDbContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            // Add framework services.
+            services.AddMvc();
+            //------ 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
